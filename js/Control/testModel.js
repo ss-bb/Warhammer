82e8bd8442e339add0953cfb092c2eda4674e6f5
olivier <goyon.olivier@gmail.com>
2014-10-06 10:51:03 +0200

    Changement structure
    Change on structure, "test" on model

---
+++ js/Control/testModel.js
@@ -0,0 +1,31 @@
+/**
+ * Created by olivier.goyon on 06/10/2014.
+ */
+//test de création de UNIT
+var troll = new Unit({M:5,CC:5,CT:5,F:5,E:5,Pv:5,I:5,A:5,Cd:10});
+var gobelin = new Unit();
+
+//test de création de l'infoUnit
+var InfoTroll = new FullUnitIfno({Unit:troll,PointCost:35,Race:"ogre"});
+
+//test de création d'élément.
+var forest = new Element({slow:1,protect:1});
+
+//test de création d'équipement et d'ajout a l'unité.
+var armor = new Equipement({armor:2});
+troll.addEquipement(armor);
+
+//test de la troupe
+var uniteogre = new Troop({unit:troll,number:3});
+uniteogre.addEquipement(armor);
+
+
+
+console.log("troll :"+troll);
+console.log("gobelin : "+gobelin);
+console.log("InfoTroll : "+InfoTroll);
+console.log("forest : "+forest);
+console.log("armor : "+armor);
+console.log("uniteogre : "+uniteogre);
+
+
--- js/Model/Perso.js
+++ js/Model/Perso.js
@@ -81,26 +81,3 @@ Troop = Backbone.Model.extend({
     }
 });
 
-//test de création de UNIT
-
-var troll = new Unit({M:5,CC:5,CT:5,F:5,E:5,Pv:5,I:5,A:5,Cd:10});
-var gobelin = new Unit();
-
-//test de création de l'infoUnit
-var InfoTroll = new FullUnitIfno({Unit:troll,PointCost:35,Race:"ogre"});
-
-//test de création d'élément.
-var forest = new Element({slow:1,protect:1});
-
-//test de création d'équipement et d'ajout a l'unité.
-var armor = new Equipement({armor:2});
-troll.addEquipement(armor);
-
-//test de la troupe
-var uniteogre = new Troop({unit:troll,number:3});
-uniteogre.addEquipement(armor);
-
-
-
-console.log("troll :"+troll);
-console.log("gobelin : "+gobelin);
